59b0ad51e665df2a583520a32951a8c9c6189228
이수린 <leesurin99@gmail.com>
2021-09-29 00:39:34 +0900

    FBV로 페이지 만들기

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,6 @@ class Post(models.Model):
 
     def __str__(self): # post로 만들어진 오브젝트 하나하나를 의미
         return f'[{self.pk}]{self.title}'
+
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}/'
