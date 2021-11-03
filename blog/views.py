3932225f13ea1cb365b110581ddd5fe8b4af9559
이수린 <leesurin99@gmail.com>
2021-11-03 14:34:18 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post, Category, Tag
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 ## CBV ##
+class PostCreate(LoginRequiredMixin, CreateView):
+    model = Post
+
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', 'tags']
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/')
 class PostList(ListView):
     model = Post
     ordering = '-pk'
@@ -23,6 +36,7 @@ class PostDetail(DetailView):
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
+
 # post_detail.html // post = model name
 ## FBV ##
 def category_page(request, slug):
