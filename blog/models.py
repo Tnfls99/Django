45af6a1de5ba1c1ef35982d5f76c4227e0bb0a7b
Surin Lee <leesurin99@gmail.com>
2021-11-10 02:24:34 +0900

    crispy_forms와 markdown 라이브러리 이용하기

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 import os
+from markdownx.models import MarkdownxField
+from markdownx.utils import markdown
 
 # Create your models here.
 class Tag(models.Model):
@@ -30,7 +32,7 @@ class Post(models.Model): ## Model 업그레이드 할 때마다 migrate 잊지 
     ## 필드선언하는곳
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
-    content = models.TextField()
+    content = MarkdownxField()
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d', blank=True)
@@ -44,7 +46,6 @@ class Post(models.Model): ## Model 업그레이드 할 때마다 migrate 잊지 
     def __str__(self): # post로 만들어진 오브젝트 하나하나를 의미
         return f'[{self.pk}]{self.title} :: {self.author}'
 
-
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
 
@@ -53,3 +54,6 @@ class Post(models.Model): ## Model 업그레이드 할 때마다 migrate 잊지 
 
     def get_file_ext(self): ## 파일 학장자 가져오는 것
         return self.get_file_name().split('.')[-1] ## -1로 가장 마지막 문자인 확장자를 가져옴
+
+    def get_content_markdown(self):
+        return markdown(self.content)
