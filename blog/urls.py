b3a330a4b4140d02f7a513867d4a4c9c1b9aa939
Surin Lee <leesurin99@gmail.com>
2021-11-10 00:29:13 +0900

    포스트 수정 기능 추가하기

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     ## FBV
     #path('<int:pk>/', views.single_post_page),
     #path('', views.index), # 서버 IP/blog
+    path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('create_post/', views.PostCreate.as_view()),
     path('tag/<str:slug>', views.tag_page),
     path('category/<str:slug>', views.category_page), #서버IP/blog/category/slug
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Post, Category, Tag
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.core.exceptions import PermissionDenied
 # Create your views here.
 
 ## CBV ##
@@ -21,6 +22,19 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
             return super(PostCreate, self).form_valid(form)
         else:
             return redirect('/blog/')
+
+class PostUpdate(LoginRequiredMixin, UpdateView): # 모델명_form 템플릿명으로 사용
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', 'tags']
+
+    # update인 경우 별도의 이름 지정 필요
+    template_name = 'blog/post_update_form.html'
+
+    def dispatch(self, request, *args, **kwargs): # Get으로 접근했는지 Post로 접근했는지 구분해주는 함수 - 장고가 제공
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
 
 class PostList(ListView):
     model = Post
